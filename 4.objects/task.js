05222b145d7adcf0821d6741b7e47fa229a3e109
vladVashurin <vlad.vashurin@bk.ru>
2025-02-28 20:24:31 +0300

    доработка четвертого задания

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,7 +4,8 @@ function Student(name, gender, age) {
 	this.age = age;
 	this.marks = [];
 }
-
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
 Student.prototype.setSubject = function(subjectName) {
 	this.subjectName = subjectName;
 }
@@ -32,16 +33,3 @@ Student.prototype.exclude = function(reason) {
 	delete this.subjectName;
 	delete this.marks;
 }
-
-let student1 = new Student("Василиса", "женский", 19);
-student1.setSubject("Algebra");
-console.log(student1.getAverage()); // 0
-student1.addMarks(4, 5, 4, 5);
-console.log(student1.getAverage()); // 4.5
-console.log(student1);
-// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
-let student2 = new Student("Артём", "мужской", 25);
-student2.setSubject("Geometry");
-student2.exclude('плохая учёба')
-console.log(student2)
-// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
